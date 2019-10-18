b81e1ecddc9d90d9a17d8f92ee8c90ab1fd0364b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-18 15:30:29 +0000

    Deploy genesixx/genesixx.github.io to github.com/genesixx/genesixx.github.io.git:master

--- sw.js
+++ sw.js
@@ -32,7 +32,7 @@ self.__precacheManifest = [
     "url": "commons.db58869303e269d6bb65.css"
   },
   {
-    "url": "commons-fef0a90623a7bc9f886d.js"
+    "url": "commons-8a67b8436731f92adf43.js"
   },
   {
     "url": "app-322a40e2b66dd26383ea.js"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3c3a979b48d20c36929ef09adb73dfd6"
+    "revision": "bb9b48b2617d9013b54586f5fda02fb1"
   },
   {
     "url": "manifest.webmanifest",
